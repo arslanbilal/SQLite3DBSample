91892d834a576f8a12f4a8b49224737dc7eb9ab3
Bilal <bilal.arslan1993@hotmail.com>
2014-07-05 23:54:40 +0300

    Initial Commit
    Nothing has done yet.

---
+++ SQLite3DBSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SQLite3DBSample
+//
+//  Created by Bilal ARSLAN on 05/07/14.
+//  Copyright (c) 2014 Bilal ARSLAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
